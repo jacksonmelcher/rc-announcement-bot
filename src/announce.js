8a3dc8599de23a1c6efceda05b8eced54bcbf27a
Jackson Melcher <jackson.melcher@ringcentral.com>
2021-01-15 11:45:19 -0800

    sending message

--- src/announce.js
+++ src/announce.js
@@ -6,12 +6,13 @@ const announce = async () => {
     let services = [];
     let sorted = [];
     services = await Service.findAll({ where: { name: 'Announce' } });
-
+    services[0].dataValues.groupId;
     if (services.length > 0) {
         let res = await bot.rc.get(
             `restapi/v1.0/glip/teams/${services[0].dataValues.groupId}`
         );
         console.log(res.data.description);
+        bot.sendMessage(group.id, { text: res.data.description });
     } else {
         return;
     }
