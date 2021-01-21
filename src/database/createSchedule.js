4e1991101ed765217c02415bbe692f1d8d9ef969
Jackson Melcher <jackson.melcher@ringcentral.com>
2021-01-21 14:15:53 -0800

    added a create methode

---
+++ src/database/createSchedule.js
@@ -0,0 +1,28 @@
+import { Service } from 'ringcentral-chatbot/dist/models';
+import log4js from 'log4js';
+
+log4js.configure({
+    appenders: { out: { type: 'stdout' } },
+    categories: { default: { appenders: ['out'], level: 'info' } },
+});
+const logger = log4js.getLogger('CREATE PROFILE');
+
+export const createSchedule = async (event, expression, message, options) => {
+    const { bot, group, userId } = event;
+
+    logger.info('CREATING PROFILE ......');
+
+    await Service.create({
+        name: `Announce`,
+        groupId: group.id,
+        botId: bot.id,
+        userId: userId,
+        data: {
+            expression,
+            message,
+            options,
+        },
+    });
+
+    logger.info('PROFILE CREATED.');
+};
